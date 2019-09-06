cb53bcd800114796657fcbe73b5f009fac881523
RUI GUO <42037392+guorui9016@users.noreply.github.com>
2019-09-06 11:32:29 +1000

    Update schedule.php
    Demo list crate by Rui. Only for test. Will be delete later if need.

--- schedule.php
+++ schedule.php
@@ -5,6 +5,24 @@
 </header>
 <body>
     <?php include("resources/layout/header.html"); ?>
+
+    // Demo list crate by Rui. Only for test. Will be delete later if need.
+    <?php
+        require '/resources/php/dbConnectOrangeWolves.php';
+        $sql= "SELECT * FROM events";
+        $result = mysqli_query($conn, $sql) or die("Error Events information - ".mysqli_error($conn));
+        while($row = mysqli_fetch_array($result)){
+            $eventName = $row["name"];
+            $description = $row["description"];
+            $date = $row["date"];
+            $teamID = $row["teamID"];
+            $busID = $row["busID"];
+            echo "<h2>$eventName</h2>";
+            echo "<h3>$date</h3>";
+            echo "<p>$description</p><br><br>";
+        }     
+    ?>
+    //above code for test. 
 </body>
 
 <?php include("resources/layout/footer.html"); ?>
