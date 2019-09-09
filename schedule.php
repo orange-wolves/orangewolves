7565eccf1d42264c4a72d315b753725fa86ce2f8
ibby97 <42018205+ibby97@users.noreply.github.com>
2019-09-09 14:43:42 +1000

    Update schedule.php
    Schedule Function is now working, it is 3 days event so i  created 3 different tables and the dates are read from sql and put in the right table to display.

--- schedule.php
+++ schedule.php
@@ -6,12 +6,15 @@
 <body>
     <?php include("resources/layout/header.html"); ?>
 
-    // Demo list crate by Rui. Only for test. Will be delete later if need.
+    // Demo list created by Ibrahim and Asim. still being worked on.
     <?php
         require '/resources/php/dbConnectOrangeWolves.php';
         $sql= "SELECT * FROM events";
         $result = mysqli_query($conn, $sql) or die("Error Events information - ".mysqli_error($conn));
-        while($row = mysqli_fetch_array($result)){
+      
+	 /*
+	 Old code will delete later.
+	 while($row = mysqli_fetch_array($result)){
             $eventName = $row["name"];
             $description = $row["description"];
             $date = $row["date"];
@@ -20,9 +23,80 @@
             echo "<h2>$eventName</h2>";
             echo "<h3>$date</h3>";
             echo "<p>$description</p><br><br>";
-        }     
+     
+
+	 }  */
+	 
+  echo "<table border=1>";
+           echo "<tr><td> Day 1</td><td>Description</tr>";
+           
+		   while ($row = mysqli_fetch_array($result))
+           {
+			  
+			  if ($row['date'] =="2019-11-15") 
+			  {
+				  
+               echo "<tr>";
+               echo "<td >$row[date] $row[name]</td>";
+			   echo "<td >$row[description]</td>";
+               echo "</tr>";
+			  }
+			  
+           } 
+		   
+           echo "</table>";
+		   
+		   echo "<br></br>";
+		   
+		    $result = mysqli_query($conn, $sql) or die("Error Events information - ".mysqli_error($conn));
+		  
+		  echo "<table border=1>";
+           echo "<tr><td>Day 2</td><td>Description</tr>";
+           
+		   while ($row = mysqli_fetch_array($result))
+           {
+			  
+			  if ($row['date'] =="2019-11-16") 
+			  {
+				  
+               echo "<tr>";
+               echo "<td >$row[date] $row[name]</td>";
+			   echo "<td >$row[description]</td>";
+               echo "</tr>";
+			  }
+			  
+           } 
+		   
+           echo "</table>";
+		   
+		    echo "<br></br>";
+		   
+		    $result = mysqli_query($conn, $sql) or die("Error Events information - ".mysqli_error($conn));
+		   echo "<table border=1>";
+           echo "<tr><td>Day 3</td><td>Description</tr>";
+           
+		   while ($row = mysqli_fetch_array($result))
+           {
+			  
+			  if ($row['date'] =="2019-11-17") 
+			  {
+				
+               echo "<tr>";
+               echo "<td >$row[date] $row[name]</td>";
+			   echo "<td >$row[description]</td>";
+               echo "</tr>";
+			  }
+			  
+			  
+           } 
+		   
+           echo "</table>";
+		   
+		   
+           mysqli_close($conn);
+		
     ?>
-    //above code for test. 
+    
 </body>
 
 <?php include("resources/layout/footer.html"); ?>
