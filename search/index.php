d39bb71cd4a488d6b423b895cc2514b2fa3caecd
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-09-09 15:07:44 +1000

    Update index.php

--- search/index.php
+++ search/index.php
@@ -29,13 +29,17 @@
          $searchtext = trim($searchtext);
          $searchtext = strip_tags($searchtext);
          $searchtext = addslashes($searchtext);
+         $textlength = 200;//This relates to the max characters shown on the description
+
+         $text; //Area to hold text for modification 
+
          $endlink;
 		   $results = 0;
 
          echo "<p>Following results for: $visabletext</p>";
          echo "<p id=final></p>";//Information is shown here is there are results
 
-         $sql =  "SELECT orangewolves.bus.busName
+         $sql =  "SELECT orangewolves.bus.busName, orangewolves.bus.description
                   FROM orangewolves.bus 
                   WHERE orangewolves.bus.busName LIKE '%$searchtext%';";
          //echo "<p>$sql</p>";
@@ -45,96 +49,118 @@
          {
             $endlink = str_replace(' ', '-', $line_array[0]);
             echo "<a href=\"../buses/$endlink\"><div>";
-            echo "<table><tr>";
-            echo "<td>$line_array[0]</td>";
-            echo "</table></tr>";
+            echo "<table>";
+            echo "<tr><td><h3>".stringWordBold($line_array[0],$searchtext)."</h3></td></tr>";//Title
+            $text = $line_array[1];//Descriptions
+            $text = formatShownString($text, $searchtext, $textlength);
+            echo "<tr><td><p>".stringWordBold($text,$searchtext)."</p></td></tr>";
+            echo "</table>";
             echo "</div></a>";
             $results++;
          }
 
-         $sql =  "SELECT orangewolves.events.name
+         $sql =  "SELECT orangewolves.events.name, orangewolves.events.description
                   FROM orangewolves.events
                   WHERE orangewolves.events.name LIKE '%$searchtext%';";
          //echo "<p>$sql</p>";
-         $result = mysqli_query($conn, $sql) or die("Error searching record - ".mysqli_error($conn));
+         $result = mysqli_query($conn, $sql) or die("Error searching record 1 - ".mysqli_error($conn));
 
          while ($line_array = mysqli_fetch_array($result))
          {
             $endlink = str_replace(' ', '-', $line_array[0]);
             echo "<a href=\"../event-info\"><div>";
-            echo "<table><tr>";
-            echo "<td><p>$line_array[0]</p></td>";
-            echo "</table></tr>";
+            echo "<table>";
+            echo "<tr><td><h3>".stringWordBold($line_array[0],$searchtext)."</h3></td></tr>";//Title
+            $text = $line_array[1];//Descriptions
+            $text = formatShownString($text, $searchtext, $textlength);
+            echo "<tr><td><p>".stringWordBold($text,$searchtext)."</p></td></tr>";
+            echo "</table>";
             echo "</div></a>";
             $results++;
          }
 
-         $sql =  "SELECT orangewolves.teams.roles
+         $sql =  "SELECT orangewolves.teams.roles, orangewolves.teams.description, orangewolves.contact.name
                   FROM orangewolves.contact
                      INNER JOIN orangewolves.teams ON orangewolves.contact.nameID = orangewolves.teams.nameID
                   WHERE orangewolves.contact.name LIKE '%$searchtext%' OR orangewolves.teams.roles LIKE '%$searchtext%';";
          //echo "<p>$sql</p>";
-         $result = mysqli_query($conn, $sql) or die("Error searching record - ".mysqli_error($conn));
+         $result = mysqli_query($conn, $sql) or die("Error searching record 2 - ".mysqli_error($conn));
 
          while ($line_array = mysqli_fetch_array($result))
          {
             $endlink = str_replace(' ', '-', $line_array[0]);
             echo "<a href=\"../teams/$endlink\"><div>";
-            echo "<table><tr>";
-            echo "<td>$line_array[0]</td>";
-            echo "</table></tr>";
+            echo "<table>";
+            echo "<tr><td><h3>".stringWordBold($line_array[0],$searchtext)."</h3></td></tr>";//Title
+            $text = $line_array[1];//Descriptions
+            $text = formatShownString($text, $searchtext, $textlength);
+            echo "<tr><td><p>".stringWordBold($text,$searchtext)."</p></td></tr>";
+            $text = $line_array[2];//Contacts Names
+            if (stripos($text, $searchtext) !== false){
+               echo "<tr><td>team members: ...".stringWordBold($text,$searchtext)."</td></tr>";
+            }
+            echo "</table>";
             echo "</div></a>";
             $results++;
          }
 
-         $sql =  "SELECT orangewolves.bus.description, orangewolves.bus.busname
+         $sql =  "SELECT orangewolves.bus.busname, orangewolves.bus.description
                   FROM orangewolves.bus
                   WHERE orangewolves.bus.description LIKE '%$searchtext%' AND orangewolves.bus.busname NOT like '%$searchtext%';";
          //echo "<p>$sql</p>";
-         $result = mysqli_query($conn, $sql) or die("Error searching record - ".mysqli_error($conn));
+         $result = mysqli_query($conn, $sql) or die("Error searching record 3 - ".mysqli_error($conn));
 
          while ($line_array = mysqli_fetch_array($result))
          {
-            $endlink = str_replace(' ', '-', $line_array[1]);
+            $endlink = str_replace(' ', '-', $line_array[0]);
             echo "<a href=\"../buses/$endlink\"><div>";
-            echo "<table><tr>";
-            echo "<td>$line_array[0]</td>";
-            echo "</table></tr>";
+            echo "<table>";
+            echo "<tr><td><h3>".stringWordBold($line_array[0],$searchtext)."</h3></td></tr>";//Title
+            $text = $line_array[1];//Descriptions
+            $text = formatShownString($text, $searchtext, $textlength);
+            echo "<tr><td><p>".stringWordBold($text,$searchtext)."</p></td></tr>";
+            echo "</table>";
             echo "</div></a>";
             $results++;
          }
 
-         $sql =  "SELECT orangewolves.events.description, orangewolves.events.name
+         $sql =  "SELECT orangewolves.events.name, orangewolves.events.description
                   FROM orangewolves.events
                   WHERE orangewolves.events.description LIKE '%$searchtext%' AND orangewolves.events.name NOT like '%$searchtext%';";
          //echo "<p>$sql</p>";
-         $result = mysqli_query($conn, $sql) or die("Error searching record - ".mysqli_error($conn));
+         $result = mysqli_query($conn, $sql) or die("Error searching record 4 - ".mysqli_error($conn));
 
          while ($line_array = mysqli_fetch_array($result))
          {
             $endlink = str_replace(' ', '-', $line_array[1]);
             echo "<a href=\"../event-info\"><div>";
-            echo "<table><tr>";
-            echo "<td>$line_array[0]</td>";
-            echo "</table></tr>";
+            echo "<table>";
+            echo "<tr><td><h3>".stringWordBold($line_array[0],$searchtext)."</h3></td></tr>";//Title
+            $text = $line_array[1];//Descriptions
+            $text = formatShownString($text, $searchtext, $textlength);
+            echo "<tr><td><p>".stringWordBold($text,$searchtext)."</p></td></tr>";
+            echo "</table>";
             echo "</div></a>";
             $results++;
          }
 
-         $sql =  "SELECT orangewolves.teams.description, orangewolves.teams.roles
+         $sql =  "SELECT orangewolves.teams.roles, orangewolves.teams.description
                   FROM orangewolves.contact
                      INNER JOIN orangewolves.teams ON orangewolves.contact.nameID = orangewolves.teams.nameID
                   WHERE orangewolves.teams.description LIKE '%$searchtext%' AND orangewolves.teams.roles NOT like '%$searchtext%' AND orangewolves.contact.name NOT like '%$searchtext%';";
          //echo "<p>$sql</p>";
-         $result = mysqli_query($conn, $sql) or die("Error searching record - ".mysqli_error($conn));
+         $result = mysqli_query($conn, $sql) or die("Error searching record 5 - ".mysqli_error($conn));
 
          while ($line_array = mysqli_fetch_array($result))
          {
-            $endlink = str_replace(' ', '-', $line_array[1]);
+            $endlink = str_replace(' ', '-', $line_array[0]);
             echo "<a href=\"../teams/$endlink\"><div>";
-            echo "<table><tr>";
-            echo "<td>$line_array[0]</td>";
-            echo "</table></tr>";
+            echo "<table>";
+            echo "<tr><td><h3>".stringWordBold($line_array[0],$searchtext)."</h3></td></tr>";//Title
+            $text = $line_array[1];//Descriptions
+            $text = formatShownString($text, $searchtext, $textlength);
+            echo "<tr><td><p>".stringWordBold($text,$searchtext)."</p></td></tr>";
+            echo "</table>";
             echo "</div></a>";
             $results++;
          }
