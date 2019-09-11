f0af59413c1a56239a509e089006e923f122c9d0
Grace <54424892+Tawncat@users.noreply.github.com>
2019-09-11 15:39:52 +1000

    Footer and Layout
    Fixing stylesheet links. Fixed css class clash. NO BORDERS.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit=no>
 		<link rel="stylesheet" type="text/css" href="resources/css/layout.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/header.css">
+		<link rel="stylesheet" type="text/css" href="resources/css/footer.css">
 	</head>
 		
 	<body>
