644d46fbd84278df98090ec77d21ec51d91cabe0
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-09-16 15:34:03 +1000

    change to html, css bus portal
    Defined the css and ensure it's mobile friendly and added an image

--- buses/index.php
+++ buses/index.php
@@ -1,35 +1,13 @@
 <!DOCTYPE html>
 <html>
-
 	<head>
 		<title>Orange Wolves Bus Portal</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit=no>
+		<link rel="stylesheet" type="text/css" href="../resources/css/busPortal.css">
 		<link rel="stylesheet" type="text/css" href="../resources/css/layout.css">
-      <link rel="stylesheet" type="text/css" href="../resources/css/header.css">
-      <link rel="stylesheet" type="text/css" href="../resources/css/footer.css">
-      <link rel="stylesheet" type="text/css" href="../resources/css/search.css">
+		<link rel="stylesheet" type="text/css" href="../resources/css/header.css">
+		<link rel="stylesheet" type="text/css" href="../resources/css/footer.css">
 	</head>
-	<style>
-		.busPortal{
-            color: #2699fb;
-			background-color: #f1f8fe;
-			text-align: center;
-		}
-
-		div.busPortal{
-			border: .2em solid white;
-			padding: 5px;
-		}
-
-		.busText{
-			color: #ffffff;
-			border: .2em solid white;
-			padding: 5px;
-            background-color: #7fc4fd;
-		}		
-		</style>
-   
-		
 	<body>
 		<div class="webpage">	
 			<div class="pagecontent col-12">
@@ -44,32 +22,38 @@
 
 <!-----------------------------CONTENT------------------------------>					
 				
-                <div class="row">
-					<div class="col-3 busPortal" style='height: 36em;'>
+                <div class="row content">
+					<div class="col-3 busPortal description">
 						<p>Orange Wolves</p>
                     </div>
                     <div class="col-9">
                         <div class="row">
-							<a href="bus-1"><div class="col-4 busPortal" style='height: 18em;'>
-                            <p>Bus 1</p>
+							<a href="bus-1"><div class="col-4 busPortal busLink">
+								<div class="busImage"></div>
+								<div class="busDescription"><p><b>Bus 1</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-2"><div class="col-4 busPortal" style='height: 18em;'>
-                            <p>Bus 2</p>
+							<a href="bus-2"><div class="col-4 busPortal busLink ">
+								<div class="busImage"></div>
+								<div class="busDescription"><p><b>Bus 2</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-3"><div class="col-4 busPortal" style='height: 18em;'>
-                            <p>Bus 4</p>
-                            </div></a>
+							<a href="bus-3"><div class="col-4 busPortal busLink">
+								<div class="busImage"></div>
+								<div class="busDescription"><p><b>Bus 2</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
+							</div></a>
                         </div>
                         <div class="row">
-							<a href="bus-4"><div class="col-4 busPortal" style='height: 18em;'>
-                            <p>Bus 4</p>
-                            </div></a>
-                            <a href="bus-5"><div class="col-4 busPortal" style='height: 18em;'>
-                            <p>Bus 5</p>
+							<a href="bus-4"><div class="col-4 busPortal busLink">
+								<div class="busImage"></div>
+								<div class="busDescription"><p><b>Bus 4</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-6"><div class="col-4 busPortal" style='height: 18em;'>
-                            <p>Bus 6</p>
-                            </div></a>
+							<a href="bus-5"><div class="col-4 busPortal busLink">
+								<div class="busImage"></div>
+								<div class="busDescription"><p><b>Bus 5</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
+							</div></a>
+							<a href="bus-6"><div class="col-4 busPortal busLink">
+								<div class="busImage"></div>
+								<div class="busDescription"><p><b>Bus 6</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
+							</div></a>
                         </div>
                     </div>
                 </div><!--End Row-->
@@ -78,21 +62,27 @@
                         <div class="row busPortal" style='height: 14em;'>
                             <p>Video</p></p>
                         </div>
-                        <div class="row">
-                            <div class="col-4 busPortal" style='height: 5.5em;'>
+                        <div>
+                            <div class="busPortal" style='height: 5.5em; width: 33%; float: left'>
                                 <p>Video 1</p></p>
                             </div>
-                            <div class="col-4 busPortal" style='height: 5.5em;'>
+                            <div class="busPortal" style='height: 5.5em; width: 34%; float: left'>
                                 <p>Video 2</p></p>
                             </div>
-                            <div class="col-4 busPortal" style='height: 5.5em;'>
+                            <div class="busPortal" style='height: 5.5em; width: 33%; float: left'>
                                 <p>Video 3</p></p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 busText" style='height: 19.5em;'>
-                        <p>Flavour Text 1, Flavour Text 2,Flavour Text 3,Flavour Text 4,Flavour Text 5,Flavour Text 6,Flavour Text 7,Flavour Text 8,Flavour Text 9</p></p>
-                    </div>
+
+					<div class="col-6 busText" style="display: table; height: 19.5em; #position: relative; overflow: hidden;">
+						<div style=" #position: absolute; #top: 50%;display: table-cell; vertical-align: middle;">
+						<div  style=" #position: relative; #top: -50%">
+						<h3>Duis pharetra sit.</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a luctus enim. Proin aliquam ante et leo gravida vestibulum. Mauris sit amet dui vel nunc ullamcorper condimentum. Curabitur placerat sem vel magna imperdiet tempus. Pellentesque at dapibus justo. Vestibulum ullamcorper aliquet mollis. Phasellus vitae laoreet est. Morbi ut dolor iaculis, rutrum libero vitae, tristique mauris. Vestibulum eleifend sem a odio aliquet, id tempus felis tincidunt. Mauris ultricies, arcu nec consectetur feugiat, nunc metus feugiat diam, in dignissim turpis nisl et magna.</p>
+						</div>
+						</div>
+					</div>
                 </div>
 			    
 
