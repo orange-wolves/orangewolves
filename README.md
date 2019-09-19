d1d8dda7ba2939e2f7e61a87253ee2920a1d5d70
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-09-20 09:40:11 +1000

    Added some css changes
    Added a easily accessable class colour in the layout.css file

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ A website will be developed to provide information about the events of Orange Wo
   - [Tools recommend Before you start](#tools-recommend-before-you-start)
   - [Setup](#setup)
   - [Rules of using GitHub](#rules-of-using-github)
+  - [Website classes](#Website-classes)
   - [Development team member](#development-team-member)
+
 
 ## Organization info
 Introduces all of team. 
@@ -46,6 +48,26 @@ In this post we're laying out few rules to help our work simplify and clean up.
 * Create a new branch for every new feature.
 * Use pull requests to merge code to master branch.
 * Do not forget pull the last version before you start coding.
+
+## Website classes
+In order for the website to have a coherent look the following classes have been created. You must include \<linkrel="stylesheet"type="text/css"href="../resources/css/layout.css">. You do not have to worry about font unless you wish to use a different font.
+
+### **Grid View**
+
+The grid view is similar to tables but are easier to use for a dynamic layout, for more information on how they work vist https://www.w3schools.com/css/css_rwd_grid.asp.
+* **row** - This class represents a container that spans the length of the available space in the parent container.
+* **col-#** - This class represents a % width of the row where # symbol is replace with a number . 1 is equivalent to 8.33% of the page width while 2 is 16.66%, 3 is 25%, etc. The total col-# for the parent row should equal 12 which is equivalent to 100%.
+* **empty** - This class is added to an empty col class to ensure the space is applied correctly.
+
+### **Container classess**
+
+The container classes are repsonsable for the container appearance and are included in the layout.css file.
+* **conNone** - this class is a white background with dark blue text, a white border and a padding of 5px.
+* **conLight** - this class is a light blue background with dark blue text, a white border and a padding of 5px.
+* **conMod** - this class is a moderate blue background with dark blue text, a white border and a padding of 5px.
+* **conDark** - this class has a dark blue background with white textand a padding of 5px a white border and a padding of 5px.
+
+
 
 ## Development team member
 Lachlan, Grace, Conor, Asim, Ibrahim, Ray, Harman, Raymond
--- buses/index.php
+++ buses/index.php
@@ -3,11 +3,10 @@
 	<head>
 		<title>Orange Wolves Bus Portal</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit=no>
-		<link rel="stylesheet" type="text/css" href="../resources/css/busPortal.css">
 		<link rel="stylesheet" type="text/css" href="../resources/css/layout.css">
 		<link rel="stylesheet" type="text/css" href="../resources/css/header.css">
 		<link rel="stylesheet" type="text/css" href="../resources/css/footer.css">
-
+		<link rel="stylesheet" type="text/css" href="../resources/css/busPortal.css">
 	</head>
 	<body>
 		<div class="webpage">	
@@ -24,34 +23,34 @@
 <!-----------------------------CONTENT------------------------------>					
 				
                 <div class="row">
-					<div class="col-3 busPortal Orangedescription">
+					<div class="col-3 conLight Orangedescription">
 						<p>Orange Wolves</p>
                     </div>
                     <div class="col-9">
                         <div class="row">
-							<a href="bus-1"><div class="col-4 busPortal busLink">
+							<a href="bus-1"><div class="col-4 conLight busLink">
 								<div class="ImageContainer"><img class="busImage" src="../resources/images/placeholder.svg" alt="bus"></div>
 								<div class="busDescription"><p><b>Bus 1</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-2"><div class="col-4 busPortal busLink ">
+							<a href="bus-2"><div class="col-4 conLight busLink ">
 								<div class="ImageContainer"><img class="busImage" src="../resources/images/placeholder.svg" alt="bus"></div>
 								<div class="busDescription"><p><b>Bus 2</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-3"><div class="col-4 busPortal busLink">
+							<a href="bus-3"><div class="col-4 conLight busLink">
 								<div class="ImageContainer"><img class="busImage" src="../resources/images/placeholder.svg" alt="bus"></div>
 								<div class="busDescription"><p><b>Bus 2</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
                         </div>
                         <div class="row">
-							<a href="bus-4"><div class="col-4 busPortal busLink">
+							<a href="bus-4"><div class="col-4 conLight busLink">
 								<div class="ImageContainer"><img class="busImage" src="../resources/images/placeholder.svg" alt="bus"></div>
 								<div class="busDescription"><p><b>Bus 4</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-5"><div class="col-4 busPortal busLink">
+							<a href="bus-5"><div class="col-4 conLight busLink">
 								<div class="ImageContainer"><img class="busImage" src="../resources/images/placeholder.svg" alt="bus"></div>
 								<div class="busDescription"><p><b>Bus 5</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
-							<a href="bus-6"><div class="col-4 busPortal busLink">
+							<a href="bus-6"><div class="col-4 conLight busLink">
 								<div class="ImageContainer"><img class="busImage" src="../resources/images/placeholder.svg" alt="bus"></div>
 								<div class="busDescription"><p><b>Bus 6</b><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis quam non augue convallis tincidunt.</p></div>
 							</div></a>
@@ -63,7 +62,7 @@
 						<?php include("../resources/videos/videoSlider.html"); ?>
                     </div>
 
-					<div class="col-6 busText flex-center" style="flex-grow: 1;">
+					<div class="col-6 conMod flex-center" style="flex-grow: 1;">
 						<div style='padding: 0.5em;'>
 							<h3>Duis pharetra sit.</h3>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a luctus enim. Proin aliquam ante et leo gravida vestibulum. Mauris sit amet dui vel nunc ullamcorper condimentum. Curabitur placerat sem vel magna imperdiet tempus. Pellentesque at dapibus justo. Vestibulum ullamcorper aliquet mollis. Phasellus vitae laoreet est. Morbi ut dolor iaculis, rutrum libero vitae, tristique mauris. Vestibulum eleifend sem a odio aliquet, id tempus felis tincidunt. Mauris ultricies, arcu nec consectetur feugiat, nunc metus feugiat diam, in dignissim turpis nisl et magna.</p>	
