79dcdef81f76a4e1997e503e011e69b04cd9d7ba
raywins <54346109+raywins@users.noreply.github.com>
2019-09-22 20:26:30 +1000

    started wireframe for teams portal
    basic layout

--- teams/index.php
+++ teams/index.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" type="text/css" href="../resources/css/layout.css">
         <link rel="stylesheet" type="text/css" href="../resources/css/header.css">
         <link rel="stylesheet" type="text/css" href="../resources/css/footer.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/team.css">
     </head>
     
     <body>
@@ -23,6 +24,79 @@
 <!-----------------------------CONTENT------------------------------>					
 				
 				<div class="row">
+            <div class="col-3 leftbar">
+                  <h1>Teams</h1>
+            </div>
+              
+            <div class="col-9 teamup" >
+                  <h2>teamup</h2>
+            </div>
+
+            <div class="col-1 teamleft" >
+                  <h2>teamleft</h2>
+            </div>
+      
+            
+            
+                <div class="col-9 teamblock">
+                    <div class="col-9 team" >
+                        <h2>team1</h2>
+                    </div>
+
+                    <div class="col-9 team">
+                        <h2>team1</h2>
+                    </div>
+
+                    <div class="col-9 team">
+                        <h2>team1</h2>
+                    </div>
+                            
+                    <div class="col-9 team">
+                        <h2>team1</h2>
+                    </div>
+                            
+                    <div class="col-9 team">
+                        <h2>team1</h2>
+                    </div>
+                            
+                    <div class="col-9 team">
+                        <h2>team1</h2>
+                    </div>
+                            
+                      <div class="col-9 team">
+                        <h2>team1</h2>
+                    </div>
+                   
+                </div>
+
+                <div class="col-1 teamright" >
+                        <h2>teamright</h2>
+                </div>
+
+
+                <div class="col-9 teamdown" >
+                        <h2>teamdown</h2>
+                </div>
+
+        </div>          
+      
+        <div class="row">
+                  
+          <div class="block col-6">
+            <h1>video</h1>
+          </div>
+
+          <div class="block col-6">
+          <h1>some text </h1>
+          </div>
+        </div>
+        
+
+                        
+                 
+       
+
+        <div class="row">
                     <a href="building-design">Building Design</a><br>
                     <a href="fashion">Fashion</a><br>
                     <a href="graphic-design">Graphic Design</a><br>
@@ -33,7 +107,7 @@
                     <a href="music">Music</a><br>
                     <a href="photography">Photography</a><br>
                     <a href="sound-production">Sound Production</a><br>
-                </div>
+          </div>
 
 <!-----------------------------FOOTER------------------------------>
 				
