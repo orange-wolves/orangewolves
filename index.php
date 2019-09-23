16743ebe5f09a6752bd012b5ca0c6bb8109e4765
lachashton <55067030+lachashton@users.noreply.github.com>
2019-09-23 15:08:20 +1000

    updated homepage

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<link rel="stylesheet" type="text/css" href="resources/css/layout.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/header.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/footer.css">
+		<link rel="stylesheet" type="text/css" href="resources/css/indexLayout.css">
 	</head>
 		
 	<body>
@@ -23,12 +24,79 @@
 
 <!-----------------------------CONTENT------------------------------>					
 				
+							<!------HERO------>	
 				<div class="row">
-					<h1>Homepage</h1>
-
-
-
+					<div class= "col-12 container-hero">
+						
+						<div class="col-12 hero-overlay">
+						
+						</div>	
+							<div class="col-4 hero-txt">
+								<h2> Orange Wolves</h2>
+								
+								<p class="hero-p-font tab-1"> This is the orange wolves home page..</p>
+							</div>
+					</div>
 				</div>
+
+							<!------INFO------>	
+				<div class="row">
+					<div class="info-box-container">
+						<div class="col-5 info-box-1-left conLight">
+
+						</div>
+						<div class="col-5 info-box-1-right conLight">
+
+						</div>
+					</div>
+				</div>		
+
+				<div class="row">
+					<div class="info-box-container">
+						<div class="col-5 info-box-2-left conLight">
+
+						</div>
+						<div class="col-5 info-box-2-right feature-info">
+
+						</div>
+					</div>
+				</div>	
+				
+				<div class="row">
+					<div class="info-box-container">
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+					
+					</div>
+				</div>	
+
+				<div class="row">
+					<div class="info-box-container">
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+						<div class="col-3 small-info-box conLight">
+
+						</div>
+					</div>
+				</div>	
+
 
 <!-----------------------------FOOTER------------------------------>
 				
@@ -42,4 +110,4 @@
 			</div>
 		</div>
 	</body>
-</html>
+</html> 
