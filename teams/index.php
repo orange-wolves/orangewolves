2f81955b95296420c76a5d06b8fe521aba034fb4
raywins <54346109+raywins@users.noreply.github.com>
2019-09-23 19:12:03 +1000

    team portal
    changed the team portal page to a 4x3 block

--- teams/index.php
+++ teams/index.php
@@ -23,91 +23,92 @@
 
 <!-----------------------------CONTENT------------------------------>					
 				
-				<div class="row">
-            <div class="col-3 leftbar">
-                  <h1>Teams</h1>
-            </div>
-              
-            <div class="col-9 teamup" >
-                  <h2>teamup</h2>
-            </div>
-
-            <div class="col-1 teamleft" >
-                  <h2>teamleft</h2>
-            </div>
-      
+		<div class="wrapper">	
             
             
-                <div class="col-9 teamblock">
-                    <div class="col-9 team" >
-                        <h2>team1</h2>
-                    </div>
+            <div class="row ">
 
-                    <div class="col-9 team">
-                        <h2>team1</h2>
-                    </div>
-
-                    <div class="col-9 team">
-                        <h2>team1</h2>
-                    </div>
-                            
-                    <div class="col-9 team">
-                        <h2>team1</h2>
-                    </div>
-                            
-                    <div class="col-9 team">
-                        <h2>team1</h2>
-                    </div>
-                            
-                    <div class="col-9 team">
-                        <h2>team1</h2>
-                    </div>
-                            
-                      <div class="col-9 team">
-                        <h2>team1</h2>
-                    </div>
-                   
+                <div class="col-3 leftbar wrapper">
+                    <h1>Teams</h1>
                 </div>
+                             
+                <div class="col-9 teamblock ">
+                    
+                    <div class="row">
+                        <div class="col-3 team " >
+                            <h2>team1</h2>
+                        </div>
 
-                <div class="col-1 teamright" >
-                        <h2>teamright</h2>
-                </div>
+                        <div class="col-3 team ">
+                            <h2>team2</h2>
+                        </div>
+
+                        <div class="col-3 team ">
+                            <h2>team3</h2>
+                        </div>
+                                
+                        <div class="col-3 team ">
+                            <h2>team4</h2>
+                        </div>
+                    </div>   
+                    
+                    <div class="row ">
+                        <div class="col-3 team " >
+                            <h2>team5</h2>
+                        </div>
+
+                        <div class="col-3 team ">
+                            <h2>team6</h2>
+                        </div>
+
+                        <div class="col-3 team ">
+                            <h2>team7</h2>
+                        </div>
+                                
+                        <div class="col-3 team ">
+                            <h2>team8</h2>
+                        </div>
+                    </div>  
+
+                    <div class="row">
+                        <div class="col-3 team" >
+                            <h2>team9</h2>
+                        </div>
+
+                        <div class="col-3 team ">
+                            <h2>team10</h2>
+                        </div>
+
+                        <div class="col-3 team ">
+                            <h2>team11</h2>
+                        </div>
+                                
+                        <div class="col-3 team ">
+                            
+                        </div>
+                    </div>              
 
 
-                <div class="col-9 teamdown" >
-                        <h2>teamdown</h2>
-                </div>
+            </div>
 
-        </div>          
-      
-        <div class="row">
-                  
-          <div class="block col-6">
-            <h1>video</h1>
-          </div>
+                         
+           
+            <div class="row">
+                        <div class="col-12 wrapper ">
+                        <a href="building-design">Building Design</a><br>
+                        <a href="fashion">Fashion</a><br>
+                        <a href="graphic-design">Graphic Design</a><br>
+                        <a href="hair-makeup">Hair + Makeup</a><br>
+                        <a href="interior-design">Interior Design</a><br>
+                        <a href="it">IT</a><br>
+                        <a href="media">Media</a><br>
+                        <a href="music">Music</a><br>
+                        <a href="photography">Photography</a><br>
+                        <a href="sound-production">Sound Production</a><br>
+                        </div>
+            </div>
 
-          <div class="block col-6">
-          <h1>some text </h1>
-          </div>
-        </div>
-        
-
-                        
-                 
-       
-
-        <div class="row">
-                    <a href="building-design">Building Design</a><br>
-                    <a href="fashion">Fashion</a><br>
-                    <a href="graphic-design">Graphic Design</a><br>
-                    <a href="hair-makeup">Hair + Makeup</a><br>
-                    <a href="interior-design">Interior Design</a><br>
-                    <a href="it">IT</a><br>
-                    <a href="media">Media</a><br>
-                    <a href="music">Music</a><br>
-                    <a href="photography">Photography</a><br>
-                    <a href="sound-production">Sound Production</a><br>
-          </div>
+</div>      
 
 <!-----------------------------FOOTER------------------------------>
 				
