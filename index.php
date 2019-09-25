270a05ebc33263e39e2e955ca5ab64b6909790ad
lachashton <55067030+lachashton@users.noreply.github.com>
2019-09-25 13:40:34 +1000

    implemented new css to make it cleaner and more functional

--- index.php
+++ index.php
@@ -25,112 +25,113 @@
 <!-----------------------------CONTENT------------------------------>					
 				
 							<!------HERO------>	
-				<div class="row">
-					<div class= "col-12 container-hero">
-						
-						<div class="col-12 hero-overlay">
-						
-						</div>	
-							<div class="col-4 hero-txt">
-								<h2> Orange Wolves</h2>
-								
-								<p class="hero-p-font tab-1"> This is the orange wolves home page..</p>
-							</div>
-					</div>
+				<div class="row info-box-container background-l-blue">
+					<div class="col-12 info-box hero-med ">
+						<div class="col-4 xsml hero-info">
+							<h2> Orange Wolves</h2>
+							<p> This is the orange wolves home page..</p>
+						</div>
+					</div>		
 				</div>
 
-							<!------INFO------>	
-				<div class="row">
-					<div class="info-box-container">
-						<div class="col-5 info-box-1-left box-color-index">
+							<!------Information------>	
 
-						</div>
-						<div class="col-5 info-box-1-right box-color-index">
+					<!--Medium/Wide information boxes row 1 -->
+				<div class="row info-box-container">
+					<div class="col-6 info-box lrg bor-spacing background-white ">
+						<h3 class="h3-med-wide-info color-d-blue">Lorem ipsum dolor</h3>
+						<p class="p-med-wide-info color-d-blue">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie a iaculis at erat pellentesque adipiscing commodo. Condimentum mattis pellentesque id nibh tortor id. Id semper risus in hendrerit gravida rutrum quisque. Non curabitur gravida arcu ac tortor dignissim. Augue neque gravida in fermentum et. Amet risus nullam eget felis eget. Ut sem viverra aliquet eget sit amet tellus. Elementum integer enim neque volutpat ac. Accumsan lacus vel facilisis volutpat est. Congue mauris rhoncus aenean vel elit scelerisque. Habitant morbi tristique senectus et netus et malesuada fames. Nisi scelerisque eu ultrices vitae auctor eu. Viverra nam libero justo laoreet sit amet cursus.</p>
+					</div>
+					<div class="col-6 info-box lrg bor-spacing  ">
 
+					</div>
+				</div>
+					<!--Medium/Wide information boxes row 2 -->
+				<div class="row info-box-container">
+					<div class="col-6 info-box med-wide bor-spacing ">
+
+					</div>
+					<div class="col-6 info-box med-wide bor-spacing background-m-blue ">
+
+					</div>
+				</div>
+					<!--Medium/Small information boxes row 1 -->
+				<div class="row info-box-container">	
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
 						</div>
 					</div>
-				</div>		
-
-				<div class="row">
-					<div class="info-box-container">
-						<div class="col-5 info-box-2-left box-color-index">
-
-						</div>
-						<div class="col-5 info-box-2-right feature-info">
-
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
 						</div>
 					</div>
-				</div>	
-
-				<div class="info-box-container-sml">
-					<div class="row">
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
 						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-							<h3>Lorem Ipsum Dolor</h3>
-							<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-				</div>	
-
-				<div class="row">
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>
-						<div class="col-2 small-info-box box-color-index">
-							<div class="small-info-inner">
-								<h3>Lorem Ipsum Dolor</h3>
-								<p class="para-small">website</p>
-							</div>
-						</div>		
 					</div>
-				</div>	
-
-
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+				
+					<!--Medium/Small information boxes row 2 -->
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+					<div class="col-2 info-box med bor-spacing ">
+						<div class="med-info-box-head">
+							<h3 class="h3-med-sml-info">Lorem Ipsum</h3>
+							<p class="p-med-sml-info">tortor</p>
+						</div>
+					</div>
+				</div>
 <!-----------------------------FOOTER------------------------------>
 				
 				<div class="row">
