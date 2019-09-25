435931443f7b3a22e660661c30242c8b9ec93323
raywins <54346109+raywins@users.noreply.github.com>
2019-09-25 15:55:07 +1000

    Update building-design.php

--- teams/building-design.php
+++ teams/building-design.php
@@ -1,12 +1,69 @@
+
+
 <!DOCTYPE html>
 <html>
-<header>
-    <h1>Building Design</h1>
-</header>
-<body>
-    <?php include("../resources/layout/header.html"); ?><br>
-</body>
 
-<?php include("../resources/layout/footer.html"); ?>
+	<head>
+		<title>Building Design</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit=no>
+		<link rel="stylesheet" type="text/css" href="../resources/css/layout.css">
+		<link rel="stylesheet" type="text/css" href="../resources/css/header.css">
+		<link rel="stylesheet" type="text/css" href="../resources/css/footer.css">
+		<link rel="stylesheet" type="text/css" href="../resources/css/indexLayout.css">
+	</head>
+		
+	<body>
+		<div class="webpage">	
+			<div class="pagecontent col-12">
 
-</html>
+<!-----------------------------HEADER------------------------------>
+				
+				<div class="row content">
+					<header>
+						<?php include("../resources/layout/header.html"); ?>
+					</header>
+				</div>
+
+<!-----------------------------CONTENT------------------------------>	
+
+
+<div class="wrapper">
+    <div class="row">
+        <div class="col-12">
+            <h1>building design</h1>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+<!-----------------------------FOOTER------------------------------>
+				
+<div class="row">
+					<footer>
+						<?php include("../resources/layout/footer.html"); ?>
+					</footer>
+				</div>
+
+
+			</div>
+		</div>
+	</body>
+</html> 
+
