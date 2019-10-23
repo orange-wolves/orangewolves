2a5340999b8bee85cffc852a09755ace982b7572
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-10-23 11:55:21 +1100

    Implemented Map
    The map on the home page has been implemented however due to z-index working funny through the script I had to change the z-index of the header and footer

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 		<link rel="stylesheet" type="text/css" href="resources/css/header.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/footer.css">
 		<link rel="stylesheet" type="text/css" href="resources/css/pages.css">
+		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+		<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
 	</head>
 		
 	<body>
@@ -49,7 +51,7 @@
 					</div>
 					<!--Requires a google API license agreement-->
 					<div class="col-6 info-container" style="height:auto;">
-						<div id="googleMap" style="height:400px;"></div>
+						<div id="OrangeWolvesEvent" style="width: auto; height: 400px;"></div>
 					</div>
 				</div>
 					<!--Medium/Small information boxes row 1 -->
@@ -154,14 +156,17 @@
 	</body>
 
 	<script>
-		function myMap() {
-		var mapProp= {
-			center:new google.maps.LatLng(-35.279827, 149.132210),//Location of the buses
-			zoom:5,
-		};
-		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-		}
-	</script>
+	var mymap = L.map('OrangeWolvesEvent').setView([-35.279827, 149.132210], 15);
+	
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoib3Jhbmdld29sdmVzIiwiYSI6ImNrMjJpbWpscTA2ZnIzZnBmczJjNmoxa2IifQ.g_RwduUVRTEDMvgCowI95Q', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1Ijoib3Jhbmdld29sdmVzIiwiYSI6ImNrMjJpbWpscTA2ZnIzZnBmczJjNmoxa2IifQ.g_RwduUVRTEDMvgCowI95Q'
+	}).addTo(mymap);
+
+	L.marker([-35.279827, 149.132210]).addTo(mymap);
+</script>
 
 	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
 </html> 
