2e7c28ecc8a7838c64d78cbac9a213f528d7d920
Grace <54424892+Tawncat@users.noreply.github.com>
2019-11-01 11:01:21 +1100

    Privacy page
    Updated privacy policy

--- privacy.php
+++ privacy.php
@@ -47,12 +47,33 @@
 				<div class="row">
 					<div class="col-12 info-container">
 						<br>
-						<p>We collect the following information when you browse this website:</p><br>
-						<ul class="indent">
-							<li>Pages Viewed</li>
-							<li>Browser Name</li>
-							<li>Language Used</li>
-							<li>Location</li>
+						<p>
+						Your privacy is important to us. It is Orange Wolves' policy to respect your privacy regarding any 
+						information we may collect from you across our website, <a href="https://www.orangewolves.com.au/"><b>https://www.orangewolves.com.au/</b></a>.<br><br>
+
+						We only ask for personal information when we truly need it to provide a service to you. 
+						We collect it by fair and lawful means, with your knowledge and consent. We also let you know 
+						why we’re collecting it and how it will be used.<br><br>
+
+						We only retain collected information for as long as necessary to provide you with your 
+						requested service. What data we store, we’ll protect within commercially acceptable means to 
+						prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.<br><br>
+
+						We don’t share any personally identifying information publicly or with third-parties, except 
+						when required to by law.<br><br>
+
+						Our website may link to external sites that are not operated by us. Please be aware that we 
+						have no control over the content and practices of these sites, and cannot accept responsibility 
+						or liability for their respective privacy policies.<br><br>
+
+						You are free to refuse our request for your personal information, with the understanding that 
+						we may be unable to provide you with some of your desired services.<br><br>
+
+						Your continued use of our website will be regarded as acceptance of our practices around 
+						privacy and personal information. If you have any questions about how we handle user data 
+						and personal information, feel free to contact us.</><br><br>
+
+						<h3>This policy is effective as of 1 November 2019.</h3>
 						</ul>
 					</div>
 				</div>
