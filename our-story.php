33d0b3557215616a256b2571023d44c47b62ca8c
Grace <54424892+Tawncat@users.noreply.github.com>
2019-11-04 09:52:16 +1100

    Fixes
    Mobile button hover fix.
    Team page design.
    CSS cleanup.

--- our-story.php
+++ our-story.php
@@ -12,6 +12,29 @@
 
 	gtag('config', 'UA-150729305-1');
 	</script>
+	<script>
+		function myFunctionStory() {
+			document.getElementById("story-myDropdown").classList.toggle("show");
+		}
+
+		// Close the dropdown menu if the user clicks outside of it
+		window.onclick = function(event) 
+		{
+			if (!event.target.matches('.story-dropbtn')) 
+			{
+				var dropdowns = document.getElementsByClassName("story-dropdown-content");
+				var i;
+				for (i = 0; i < dropdowns.length; i++) 
+				{
+					var openDropdown = dropdowns[i];
+					if (openDropdown.classList.contains('show')) 
+					{
+						openDropdown.classList.remove('show');
+					}
+				}
+			}
+		}
+	</script>
 		<title>Our Story</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit=no>
 		<link rel="icon" href="resources/images/OrangeWolvesFavIcon.png">
@@ -43,63 +66,32 @@
 						<h1>Our Story</h1>
 						</div>
 						
-						
-						
 						<div class="col-4">
 							<div class="dropdown"name="dropdown">
-									<button onclick="myFunctionStory()" class="dropbtnn">Choose a Team Story
+									<button onclick="myFunctionStory()" class="story-dropbtn">Choose a Team Story
 									 <i class="fa fa-caret-down"></i></button>
 									<i class="down"></i>
-								
 							</div>
-                		</div>
-				
+            </div>
 
-						<div id="myDropdownn" class="dropdownn-content col-12">
-										<div class="col-6">
-											<a href="our-story.php?team_name=T1"><h2>Fashion</h2></a>
-											<a href="our-story.php?team_name=T3"><h2>Graphic Design</h2></a>
-											<a href="our-story.php?team_name=T4"><h2>Hair</h2></a>
-											<a href="our-story.php?team_name=T5"><h2>Horticulture</h2></a>
-											<a href="our-story.php?team_name=T6"><h2>Information technology</h2></a>
-											<a href="our-story.php?team_name=T7"><h2>Interior Design</h2></a>
-										</div>
-										<div class="col-6">
-											<a href="our-story.php?team_name=T8"><h2>Makeup</h2></a>
-											<a href="our-story.php?team_name=T9"><h2>Media</h2></a>
-											<a href="our-story.php?team_name=T10"><h2>Music</h2></a>
-											<a href="our-story.php?team_name=T11"><h2>Photography</h2></a>
-											<a href="our-story.php?team_name=T12"><h2>Visual Art</h2></a>
-											<a href="our-story.php?team_name=T13"><h2>Yurauna</h2></a>
-										</div>
+						<div id="story-myDropdown" class="story-dropdown-content col-12">
+							<div class="col-6">
+								<a href="our-story.php?team_name=T1"><h2>Fashion</h2></a>
+								<a href="our-story.php?team_name=T3"><h2>Graphic Design</h2></a>
+								<a href="our-story.php?team_name=T4"><h2>Hair</h2></a>
+								<a href="our-story.php?team_name=T5"><h2>Horticulture</h2></a>
+								<a href="our-story.php?team_name=T6"><h2>Information technology</h2></a>
+								<a href="our-story.php?team_name=T7"><h2>Interior Design</h2></a>
+							</div>
+							<div class="col-6">
+								<a href="our-story.php?team_name=T8"><h2>Makeup</h2></a>
+								<a href="our-story.php?team_name=T9"><h2>Media</h2></a>
+								<a href="our-story.php?team_name=T10"><h2>Music</h2></a>
+								<a href="our-story.php?team_name=T11"><h2>Photography</h2></a>
+								<a href="our-story.php?team_name=T12"><h2>Visual Art</h2></a>
+								<a href="our-story.php?team_name=T13"><h2>Yurauna</h2></a>
+							</div>
 						</div>
-							
-							
-							
-				<script>
-						function myFunctionStory() {
-							document.getElementById("myDropdownn").classList.toggle("show");
-						}
-
-						// Close the dropdown menu if the user clicks outside of it
-				window.onclick = function(event) 
-					{
-					if (!event.target.matches('.dropbtnn')) 
-					{
-						var dropdowns = document.getElementsByClassName("dropdownn-content");
-						var i;
-						for (i = 0; i < dropdowns.length; i++) 
-						{
-							var openDropdown = dropdowns[i];
-							if (openDropdown.classList.contains('show')) 
-							{
-								openDropdown.classList.remove('show');
-							}
-						}
-					}
-				}
-				</script>
-
 						</div>
 					</div>
 				</div>
