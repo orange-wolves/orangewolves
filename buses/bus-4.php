d65418a8c7edfda48f2e1362454d19d11b4b2d49
Astra078 <54424091+Astra078@users.noreply.github.com>
2019-11-20 11:39:43 +1100

    Updated bus 4 and css
    bus 4 now has content while the css has been updated to 1.2 and changed in the style sheet

--- buses/bus-4.php
+++ buses/bus-4.php
@@ -33,14 +33,46 @@
 									<div id='busicon'>
 										<img src="../resources/images/MusicBus.svg" alt="Photography & Building Design Bus" >
 									</div>
-									<h1>Photography & Building Design</h1>
-										<p>Information to be added</p>
+                                    <h1>Photography & Building Design</h1>
+                                    <p><b>Building Design</b> - Diploma<br><b>Photography</b> - Diploma</p><br>
+                                    <h2>Competition</h2>
+                                    <h2>17th December</h2>
+
 								</div>
 
 								<div class="col-8 businfo-content">
-<h1>Information to be added</h1>
+                                <h1>Building Design</h1>
+                                <p>This bus is showcasing a selection of tiny house designs that our first year Diploma of Building Design students have designed as part of their design studio class. Why tiny houses? Tiny houses offer many benefits, cater for a certain demographic and fill a niche in the housing supply.</p>
+                                <ul class="indent">
+                                    <li>Tiny houses have a small carbon footprint.</li>
+                                    <li>Often use upcycling e.g. shipping containers or recycled building materials such as cladding.</li>
+                                    <li>They are demountable and can be re-located.</li>
+                                    <li>They offer the same comfort and amenities as a standard home.</li>
+                                    <li>They can serve as secondary residence, studio, home office and guest or pool house.</li>
+                                    <li>They are affordable and have low running cost.</li>
+                                    <li>Tiny houses can be ‘off grid’.</li>
+                                </ul><br>
+                                <p>In the spirit of the tiny house principles, the Orange Wolf buses are perfectly suited for conversion into a tiny house after their end of service life.</p><br>
+                                <p>The displays show an extract of our Diploma of Building Design student’s work. The concept design sketches are done ‘free hand’, No drafting software was used at this stage.</p><br>
+                                <p>The original work was produced to a 1:50 scale. This encourages the students to think about dimensions, spatial layout and simple, ergonomical design within the confines of a bus. The students also experimented with hand rendering and the application of colour.</p>
+                                <br>
+                                <h2>Competition:</h2>
+                                <p>We will also be hosting a competition and we would like you to participate! It is your chance to design a tiny house bus and there will be first, second and third place prizes!</p><br>
+                                <p>Flat pack models of the bus will be available in the Building Design bus for those participating in the competition. There will be instructions on how to build the scaled model included in the pack.</p>
+                                <br>
+                                <h2>Design brief:</h2>
+                                <p>For the design, you must include the following;</p>
+                                <ul class="indent">
+                                    <li>Living area</li>
+                                    <li>Kitchen</li>
+                                    <li>Wet area including a toilet, sink and shower</li>
+                                    <li>Bed</li>
+                                    <li>Study area</li>
+                                </ul><br>
+                                <p>The panel of judges will be looking at whether the brief has been met, the functionality of the layout and also the quirkiness, so have fun with your design!</p><br>
+                                <p>You must submit at least one photo of your design by emailing it to Creative.Design@cit.edu.au Entries will be accepted till 9am on Monday, 16th December.</p><br>
+                                <p>The winners of the competition will be announced on the Tuesday, 17th December.</p>
 								</div>
-
 							</div>
             </div>
 
